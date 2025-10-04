6a01eeb4eb3bf7cae138841803f73c4f3c93cec2
Gabor Torok <gtorok@lbl.gov>
2025-10-04 13:06:22 -0700

    optionally hide routers with no adapter

--- app/routers/iri_router.py
+++ app/routers/iri_router.py
@@ -16,8 +16,18 @@ class IriRouter(APIRouter):
         # Load the facility-specific adapter
         router_name = self.prefix.replace("/", "").strip()
 
+        # if there is no adapter specified for this router, 
+        # and IRI_SHOW_MISSING_ROUTES is not true,
+        # hide the router
+        env_var = f"IRI_API_ADAPTER_{router_name}"
+        logging.getLogger().info(f"Loading adapter from {env_var}")
+        if env_var not in os.environ and os.environ.get("IRI_SHOW_MISSING_ROUTES") not in ["true", "1", "on", "yes"]:
+            logging.getLogger().info(f"Hiding route: {router_name}")
+            self.include_in_schema = False
+            return
+        
         # find and load the actual implementation
-        adapter_name = os.environ.get(f"IRI_API_ADAPTER_{router_name}", "app.demo_adapter.DemoAdapter")
+        adapter_name = os.environ.get(env_var, "app.demo_adapter.DemoAdapter")
         logging.getLogger().info(f"Using {router_name} adapter: {adapter_name}")
         parts = adapter_name.rsplit(".", 1)
         module = importlib.import_module(parts[0])    
